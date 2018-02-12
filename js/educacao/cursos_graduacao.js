449bdedb93102563b32fe74f7aed268e9826f815
lbella <lbella@thoughtworks.com>
2018-02-12 17:52:20 -0200

    Adds submenu to education page

---
+++ js/educacao/cursos_graduacao.js
@@ -0,0 +1,49 @@
+
+zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
+            zingchart.THEME="classic";
+             
+            var myConfig = {
+                "background-color":"#ffffff",
+                "graphset":[
+                    
+                    {
+                        "type":"wordcloud",
+                        "x":"0%",
+                        "y":"0%",
+                        "width":"100%",
+                        "height":"100%",
+                        "background-color":"white",
+                        "title":{
+                            "text":"Cursos de Graduação na computação <br>cursados pelas mulheres",
+                            "y":"5%",
+                            "background-color":"none",
+                            "font-size":"20px",
+                            "font-color":"#000000"
+                        },
+                        "plotarea":{
+                            "margin":"65px auto auto auto"
+                        },
+                        "options":{
+                            "max-items":100,
+                            "font-family":"Arial",
+                            "rotate":true,
+                            "min-length":4,
+                            "min-font-size":"9px",
+                            "max-font-size":"30px",
+                            "color-type":"palette",
+                            "palette":["#e91e63", "#00bcd4", "#9575cd", "#009688", "#9c27b0", "#e91e63", "#3f51b5"],
+                            "ignore":["com" , "o", "para", "de"],
+                            "text":"  CiênciaDaComputação CiênciaDaComputação CiênciaDaComputação CiênciaDaComputação CiênciaDaComputação SistemasDeInformação  CiênciaDaComputação SistemasDeInformação CiênciasDaComputação Administração SistemasDeInformação Matematica SistemasDeInformação EngenhariaDeComputação EngenhariaDeComputação CiênciaDaComputaçao SistemasDeInformação  EngenhariaElétrica CiênciaDaComputação SistemasDeInformação  EngenhariaDeComputação Análise&DesenvolvimentoDeSistemas SistemasDeInformação EngenhariaDeComputação AnálisedeSistemas SistemasDeInformação SistemasDeInformação  CiênciaDaComputação SistemasDeInformação CiênciaDaComputação RelaçõesPublicas CiênciaDaComputação  CiênciaDaComputação EngenhariaDeComputação DesenvolvimentoDeSistemasWeb SistemaDeInformação CiênciaDaComputação ServiçoSocial  SistemasDeInformação  CiênciaDaComputação CiênciaDaComputação  SistemasDeInformação DesingDeMultimidia AnaliseDeSistemas SistemaDeInformação  SistemasDeInformação EngenhariaDeControleAutomação  SistemaDeInformação CiênciaDaComputação EngenhariaDeComputação SistemasDaInformação  CiênciasContábeis EngenhariaDecomputação SistemasDeInformação SistemasDeInformação EngenhariaDeComputação CiênciaDaComputação  EngenhariaElétrica  TecnologiaEmProcessamentoDeDados EngenhariaDeComputação  CiênciaDaComputação CiênciaDaComputação CiênciaDaComputação SistemasDeInformação SistemasDeInformação SistemasDeInformação CiênciaDaComputação SistemasDeInformação CiênciaDaComputação CiênciaDaComputação  SistemasDeInformação CiênciaDaComputação  SistemasDeInformação  CiênciasDaComputação CiênciaDaComputação SistemasDeInformação CiênciaDaComputação CiênciaDaComputação  SistemasDeInformação SistemasDeInformação SistemasDeInformação  SistemasDeInformação ComércioExterior SistemasDeInformação CiênciaDaComputação EngenhariaDeComputação EngenhariaDeComputação CiênciaDaComputação  ArtesVisuaisComÊnfaseEmComputaçãoGráfica  SistemasDeInformação BachareladoEmInformática Estatística SistemasDeInformação  SistemasDeInformação CiênciaDaComputação  CiênciaDaComputação EngenhariaDeSistemas SistemasDeInformação AnalistaDeSistemas SistemasDeInformação  CiênciaDaComputação CiênciaDaComputação CiênciaDaComputação  CiênciaDaComputação AnáliseEDesenvolvimentoDeSistemas EngenhariaDeSoftware SistemasDeInformação CiênciaDaComputação EngenhariaDeComputação EngenhariaDeComputação JogosDigitais SistemasDeInformação SistemasDeInformação EngenhariaDeTelecomunicações CiênciaDaComputação EngenhariaDeComputação CiênciaDaComputação CiênciaDaComputação SistemasDeInformação CiênciasDaComputação CiênciaDaComputação SistemasDeInformação AnáliseDesenvolvimentoDeSistemas SistemasDeInformação CiênciaDaComputação SistemasDeInformação BusinessIntelligence CiênciaDaComputação EngenhariaDeComputação SistemaDeInformaçao SistemasDeInformação EngenhariaDeComputação AnaliseEDesenvolvimentoDeSistemas  EngenhariaDeComputação SistemasDeInformação TecnólogoEmProcessamentoDeDados CiênciaDaComputação Administração EngenhariaDeComputação SistemasDeInformação EngenhariaDeComputação EngenhariaDeComputação SistemaDeInformação  AnálisedeSistemas  CiênciasDaComputação  ProcessamentoDeDados EngenhariaDeSoftware  EngenhariaDeControleAutomação.   SistemaDeInformação CiênciaDaComputação SistemasDeInformação  CiênciasDaComputação CiênciaDaComputação SistemasDeInformação EngenhariaDeComputação CiênciaDaComputação  ProduçãoMultimídia EngenhariaQuímica  GestãoEmTI GestãoDeProjetosEmTI CiênciaDaComputação TecnologiaEmAnaliseEDesenvolvimentoDeSistemas CiênciaDaComputação SistemasDeInformação CiênciaDaComputação EngenhariaDeComputação  CiênciaDaComputação CiênciaDaComputação DesignGráfico CiênciaDaComputação SistemasParaInternet EngenhariaDeComputação  EngenhariaDeComputação  EngenhariaDeComputação CiênciaDaComputação SistemasDeInformação SistemasDeInformação CiênciaDaComputação CiênciaDaComputação SistemasDeInformação SistemasDeInformação  LicenciaturaEmInformática  SistemasDeInformação CSTC/ITA CiênciaDaComputação CiênciaDaComputação  CiênciaDaComputação CiênciaDaComputação EspecializaçãoEmBancoDeDados  CSTC/ITA SistemasDeInformação EngenhariaDeComputação SistemasDeInformação CiênciaDaComputação LicenciaturaInformática EngenhariaDeSofwtare EngenhariaDeComputação Cic CiênciaDaComputação ADS AnáliseDesenvolvimentoDeSistemas SistemasDeInformação  AdministraçãoDeEmpresas AdministraçãoDeEmpresas SistemasDeInformação SistemasDeInformação"
+                        }
+                    }
+                ]
+            }
+             
+            ; 
+             
+            zingchart.render({ 
+                id : 'cursos_graduacao', 
+                data : myConfig, 
+                height: 500, 
+                width: 900 
+            });
