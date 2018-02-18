43971ac95169641a2bca686cc7c2069d12b6357c
lbella <lbella@thoughtworks.com>
2018-02-18 17:00:28 -0300

    Changes age graph

--- js/personificacao/idades.js
+++ js/personificacao/idades.js
@@ -1,258 +1,185 @@
 var myConfig = {
- 	type: "treemap", 
-  "background-color":"#fff",
-  "title":{
-          "text":"Idade das mulheres que responderam o questionário",
-          "y":"1%",
-          "background-color":"none",
-          "font-size":"20px",
-          "font-color":"#000000"
-      },
- 	"plotarea":{
- 	  "margin":"70px 40px 40px 40px"
- 	},
- 	options: {
- 	  "max-children":[6,6,6],
- 	  "tooltip-box":{
- 	    "text":"%value mulheres com<br /> %text"
- 	      },
- 	      "aspect-type":"transition",
-    "color-start":"#ede7f6",
-    "color-end":"#673ab7"
- 	  },
-	series : [
-		 {
+  type: "pie", 
+  backgroundColor: "white",
+  plot: {
+    borderColor: "white",
+    borderWidth: 5,
+    // slice: 90,
+    valueBox: {
+      placement: 'out',
+      text: '%t<br>%npv%',
+      fontFamily: "Open Sans"
+    },
+    tooltip:{
+      fontSize: '18',
+      fontFamily: "Open Sans",
+      padding: "5 5",
+      text: "%t<br>R$%v"
+    },
+    animation:{
+      effect: 2, 
+      method: 5,
+      speed: 500,
+      sequence: 1
+    }
+  },
+  title: {
+    fontColor: "black",
+    text: 'Idade das mulheres que responderam o questionário',
+    backgroundColor: "white",
+    offsetX: 10,
+    fontSize: "20px"
+  },
+  plotarea: {
+    margin: "70px 0 0 0"  
+  },
+series : [
+     {
       text: "19 anos",
-      value: 2,
-      "data-catagory":"19 anos",
-        box:{
-          fontSize:20
-        },
+      values: [2],
+      backgroundColor: "#e57373",
       },
       {
       text: "20 anos",
-      value: 4,
-      "data-catagory":"20 anos",
-        box:{
-          fontSize:20
-        },
+      values: [4],
+      backgroundColor: "#f06292",
       },
      {
-			text: "21 anos",
-			value: 6,
-			"data-catagory":"21 anos",
-			  box:{
-			    fontSize:20
-			  },
-			},
+      text: "21 anos",
+      values: [6],
+      backgroundColor: "#ba68c8",
+      },
       {
       text: "22 anos",
-      value: 11,
-      "data-catagory":"22 anos",
-        box:{
-          fontSize:20
-        },
+      values: [11],
+      backgroundColor: "#9575cd",
       },
-  		{
+      {
       text: "23 anos",
-      value: 12,
-      "data-catagory":"23 anos",
-        box:{
-          fontSize:20
-        },
+      values: [12],
+      backgroundColor: "#7986cb",
       },
       {
       text: "24 anos",
-      value: 17,
-      "data-catagory":"24 anos",
-        box:{
-          fontSize:20
-        },
+      values: [17],
+      backgroundColor: "#64b5f6",
       },
       {
       text: "25 anos",
-      value: 15,
-      "data-catagory":"25 anos",
-        box:{
-          fontSize:20
-        },
+      values: [15],
+      backgroundColor: "#4fc3f7",
       },
       {
       text: "26 anos",
-      value: 16,
-      "data-catagory":"26 anos",
-        box:{
-          fontSize:20
-        },
+      values: [16],
+      backgroundColor: "#4dd0e1",
       },
       {
       text: "27 anos",
-      value: 18,
-      "data-catagory":"27 anos",
-        box:{
-          fontSize:20
-        },
+      values: [18],
+      backgroundColor: "#4db6ac",
       },
       {
       text: "28 anos",
-      value: 13,
-      "data-catagory":"28 anos",
-        box:{
-          fontSize:20
-        },
+      values: [13],
+      backgroundColor: "#81c784",
       },
       {
       text: "29 anos",
-      value: 17,
-      "data-catagory":"29 anos",
-        box:{
-          fontSize:20
-        },
+      values: [17],
+      backgroundColor: "#aed581",
       },
       {
       text: "30 anos",
-      value: 11,
-      "data-catagory":"30 anos",
-        box:{
-          fontSize:20
-        },
+      values: [11],
+      backgroundColor: "#dce775",
       },
       {
       text: "31 anos",
-      value: 11,
-      "data-catagory":"31 anos",
-        box:{
-          fontSize:20
-        },
+      values: [11],
+      backgroundColor: "#fff176",
       },
       {
       text: "32 anos",
-      value: 7,
-      "data-catagory":"32 anos",
-        box:{
-          fontSize:20
-        },
+      values: [7],
+      backgroundColor: "#ffd54f",
       },
       {
       text: "33 anos",
-      value: 8,
-      "data-catagory":"33 anos",
-        box:{
-          fontSize:20
-        },
+      values: [8],
+      backgroundColor: "#ffb74d",
       },
       {
       text: "34 anos",
-      value: 12,
-      "data-catagory":"34 anos",
-        box:{
-          fontSize:20
-        },
+      values: [12],
+      backgroundColor: "#ff8a65",
       },
       {
       text: "35 anos",
-      value: 9,
-      "data-catagory":"35 anos",
-        box:{
-          fontSize:20
-        },
+      values: [9],
+      backgroundColor: "#ef5350",
       },
       {
       text: "36 anos",
-      value: 2,
-      "data-catagory":"36 anos",
-        box:{
-          fontSize:20
-        },
+      values: [2],
+      backgroundColor: "#ef5350",
       },
       {
       text: "37 anos",
-      value: 5,
-      "data-catagory":"37 anos",
-        box:{
-          fontSize:20
-        },
+      values: [5],
+      backgroundColor: "#7e57c2",
       },
       {
       text: "38 anos",
-      value: 2,
-      "data-catagory":"38 anos",
-        box:{
-          fontSize:20
-        },
+      values: [2],
+      backgroundColor: "#5c6bc0",
       },
       {
       text: "39 anos",
-      value: 6,
-      "data-catagory":"39 anos",
-        box:{
-          fontSize:20
-        },
+      values: [6],
+      backgroundColor: "#42a5f5",
       },
       {
       text: "41 anos",
-      value: 6,
-      "data-catagory":"41 anos",
-        box:{
-          fontSize:20
-        },
+      values: [6],
+      backgroundColor: "#29b6f6",
       },
       {
       text: "42 anos",
-      value: 5,
-      "data-catagory":"42 anos",
-        box:{
-          fontSize:20
-        },
+      values: [5],
+      backgroundColor: "#26c6da",
       },
       {
       text: "43 anos",
-      value: 1,
-      "data-catagory":"43 anos",
-        box:{
-          fontSize:20
-        },
+      values: [1],
+      backgroundColor: "#26a69a",
       },
       {
       text: "44 anos",
-      value: 5,
-      "data-catagory":"44 anos",
-        box:{
-          fontSize:20
-        },
+      values: [5],
+      backgroundColor: "#66bb6a",
       },
       {
       text: "45 anos",
-      value: 4,
-      "data-catagory":"45 anos",
-        box:{
-          fontSize:20
-        },
+      values: [4],
+      backgroundColor: "#9ccc65",
       },
       {
       text: "46 anos",
-      value: 3,
-      "data-catagory":"46 anos",
-        box:{
-          fontSize:20
-        },
+      values: [3],
+      backgroundColor: "#d4e157",
       },
       {
       text: "63 anos",
-      value: 1,
-      "data-catagory":"63 anos",
-        box:{
-          fontSize:20
-        },
+      values: [1],
+      backgroundColor: "#ffeb3b",
       },
 ]
 };
  
 zingchart.render({ 
-	id : 'idades', 
-	data : myConfig, 
-	height: 500, 
-	width: 850,
-	hideprogresslogo: true,
-	output: 'canvas'
+  id : 'idades', 
+  data : myConfig, 
+  height: 500, 
+  width: "100%" 
 });
